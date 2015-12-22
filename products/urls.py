e4a93a8ffefc1f60162f9ee50bb7ec90a09025a9
n2o <christian.meter@uni-duesseldorf.de>
2015-12-22 19:10:30 +0100

    Define new urls

--- products/urls.py
+++ products/urls.py
@@ -1,9 +1,10 @@
+# -*- coding: utf-8 -*-
 from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^maschinensaegeblatt/$', views.sawblades, name='sawblades'),
+    url(r'^maschinensaegeblatt/$', views.sawblades, {'title': "Maschinensägeblätter"}, name='sawblades'),
     url(r'^maschinensaegeblatt/(?P<clamping>[\w-]+)/(?P<slug>[\w-]+)/$', views.sawblade, name='sawblade'),
 ]
