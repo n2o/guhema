beb743ece1e8969c4c9c6b0553e2daa99418ece6
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-01-31 14:25:50 +0100

    Include advices

--- products/urls.py
+++ products/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     url(r'^lochsagen/empfehlung$', views.holesawAdvice, name='holesaw_advice'),
     url(r'^(?P<slug>[\w-]+)/$', views.list, name='list'),
     url(r'^(?P<slug>[\w-]+)/details$', views.product_details, name='product_details'),
-    url(r'^(?P<slug>[\w-]+)/empfehlungen$', views.product_advices, name='product_advices'),
+    url(r'^(?P<slug>[\w-]+)/empfehlungen$', views.product_advices, name='advices'),
     url(r'^(?P<slug>[\w-]+)/(?P<id>[0-9]+)/$', views.details_by_id, name='details_by_id'),
     url(r'^(?P<slug>[\w-]+)/(?P<type>[\w-]+)/$', views.details, name='details'),
 ]
