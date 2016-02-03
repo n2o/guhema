76163a7e1f2c4f20be58272b48cd2085b9e58dee
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-03 17:36:45 +0100

    Add model translations

--- products/translation.py
+++ products/translation.py
@@ -1,7 +1,27 @@
 from modeltranslation.translator import register, TranslationOptions
-from .models import ProductGroup
+from .models import ProductGroup, SawBlade, HoleSaw, BandSawBlade, JigSawBlade
 
 
 @register(ProductGroup)
 class ProductGroupTranslationOptions(TranslationOptions):
     fields = ('name', 'description',)
+
+
+@register(SawBlade)
+class SawBladeTranslationOptions(TranslationOptions):
+    fields = ('description',)
+
+
+@register(HoleSaw)
+class HoleSawTranslationOptions(TranslationOptions):
+    fields = ('description', 'category',)
+
+
+@register(BandSawBlade)
+class BandSawBladeTranslationOptions(TranslationOptions):
+    fields = ('type_description', 'type2_description',)
+
+
+@register(JigSawBlade)
+class JigSawBladeTranslationOptions(TranslationOptions):
+    fields = ('subcategory',)
