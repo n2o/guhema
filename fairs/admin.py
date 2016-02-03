3524bb4eb5568b2b136bb4638b0edb25150490f2
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-03 19:43:44 +0100

    Update fair admin panel

--- fairs/admin.py
+++ fairs/admin.py
@@ -1,10 +1,19 @@
 from django.contrib import admin
+from modeltranslation.admin import TranslationAdmin
+from django.db import models
+from pagedown.widgets import AdminPagedownWidget
 
 from .models import Fair
 
 
+class PageDownAdmin(TranslationAdmin):
+    formfield_overrides = {
+        models.TextField: {'widget': AdminPagedownWidget}
+    }
+
+
 @admin.register(Fair)
-class FairAdmin(admin.ModelAdmin):
+class FairAdmin(PageDownAdmin):
     list_display = ('name', 'location', 'start', 'end', 'archive', 'public', 'created')
     list_filter = ['created']
     search_fields = ['name']
