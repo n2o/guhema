966dc313f498137afcc259e9c88d70f7ed7626c9
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-07 01:24:39 +0100

    Add translation to form fields

--- contact/forms.py
+++ contact/forms.py
@@ -1,11 +1,12 @@
 from captcha.fields import ReCaptchaField
 from django import forms
+from django.utils.translation import ugettext_lazy as _
 
 
 class EmailForm(forms.Form):
-    firstname = forms.CharField(label="Vorname", max_length=255)
-    lastname = forms.CharField(label="Nachname", max_length=255)
-    email = forms.EmailField(label="E-Mail")
-    subject = forms.CharField(label="Betreff", max_length=255)
+    firstname = forms.CharField(label=_("Vorname"), max_length=255)
+    lastname = forms.CharField(label=_("Nachname"), max_length=255)
+    email = forms.EmailField(label=_("E-Mail"))
+    subject = forms.CharField(label=_("Betreff"), max_length=255)
     captcha = ReCaptchaField()
-    message = forms.CharField(label="Deine Nachricht", widget=forms.Textarea)
+    message = forms.CharField(label=_("Deine Nachricht"), widget=forms.Textarea)
