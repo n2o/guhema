fac60be696461a9792cb3cdc5db77dd59dadeacc
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-14 15:10:14 +0100

    Add middleware to automatically set preferred language on first visit

---
+++ core/middleware.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+from django.utils import translation
+
+
+class ForceLangMiddleware:
+    @staticmethod
+    def process_request(request):
+        request.LANG = translation.get_language()
+        translation.activate(request.LANG)
+        request.LANGUAGE_CODE = request.LANG
