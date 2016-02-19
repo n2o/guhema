da3df2897a59d6199ec3ed4b16cef6d3117a0afb
Christian Meter <christian.meter@uni-duesseldorf.de>
2016-02-19 17:31:57 +0100

    Update fairs

--- fairs/views.py
+++ fairs/views.py
@@ -8,8 +8,8 @@ from .models import Fair
 def index(request):
     upcoming = Fair.objects.filter(archive=False,
                                    public=True,
-                                   end__gt=datetime.now())
+                                   end__gte=datetime.now())
     past = Fair.objects.filter(public=True,
-                               end__lt=datetime.now())
+                               end__lte=datetime.now())
     return render(request, 'fairs/index.html', {'upcoming': upcoming,
                                                 'past': past})
