5f1be83c120cdb2bfdad45c5bdeb2f8b7656e0f7
Christian Meter <cmeter@googlemail.com>
2023-01-30 22:31:13 +0100

    Add umask

--- docker_init.sh
+++ docker_init.sh
@@ -4,4 +4,9 @@ poetry run python manage.py collectstatic --noinput
 
 poetry run python manage.py migrate
 
+mkdir -p media
+cd media
+umask 011
+cd ..
+
 poetry run gunicorn --bind 0.0.0.0:8000 core.wsgi
